5452da110e1e5207869d312881ccc620ab68cf45
Gleb Ivashyn <gleb.ivashyn@stud.vgtu.lt>
2022-07-15 15:47:28 +0300

    tests

--- src/main/java/com/epam/task/purchases/Runner.java
+++ src/main/java/com/epam/task/purchases/Runner.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Runner {
-    public static int search(AbstractPurchase[] purchases) {
+    public static int search(AbstractPurchase[] purchases, Euro searchValue) {
         Comparator<AbstractPurchase> comparator = AbstractPurchase::compareTo;
 
         return Arrays.binarySearch(
                 purchases,
-                new DiscountIfMorePurchase(new Product(null, new Euro(500)), 1),
+                new DiscountIfMorePurchase(new Product(null, new Euro(searchValue)), 1),
                 comparator);
     }
 
@@ -19,7 +19,7 @@ public class Runner {
         Product iceCream = new Product("Ice cream", new Euro(190));
         Product cupcake = new Product("cupcake", new Euro(130));
         Product proteinBar = new Product("Protein bar", new Euro(310));
-        Product cookie = new Product("cookie ", new Euro(100));
+        Product cookie = new Product("cookie", new Euro(100));
         Product gummy = new Product("gummy", new Euro(117));
 
 
@@ -33,20 +33,20 @@ public class Runner {
         };
 
         for (AbstractPurchase p : purchases) {
-            System.out.println(p.getCost());
+            System.out.println(p.toString());
         }
 
         Arrays.sort(purchases);
         System.out.println("\n");
 
         for (AbstractPurchase p : purchases) {
-            System.out.println(p.getCost());
+            System.out.println(p.toString());
         }
 
         System.out.println("Minimal cost = " + purchases[purchases.length - 1].getCost());
 
-        System.out.println((search(purchases) == -1)? "No purchase with such sum":
-                "Index of purchase with sum 5.00 : " + search(purchases));
+        System.out.println((search(purchases, new Euro(500)) == -1)? "No purchase with such sum":
+                "Index of purchase with sum 5.00 : " + search(purchases, new Euro(500)));
 
     }
 
--- src/test/java/com/epam/task/purchases/task5/RunnerTest.java
+++ src/test/java/com/epam/task/purchases/task5/RunnerTest.java
@@ -1,18 +1,77 @@
 package com.epam.task.purchases.task5;
 
+import com.epam.task.purchases.*;
 import com.epam.task.purchases.base.BaseIOTest;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RunnerTest extends BaseIOTest {
 
-   
+    @Test
+    void isOutputCorrect() {
+        Runner runner = new Runner();
+
+        runner.main(null);
+
+        assertOutEquals("Chocolate bar;2.45;3\n" +
+                "Ice cream;1.90;2\n" +
+                "cupcake;1.30;6\n" +
+                "Protein bar;3.10;1\n" +
+                "cookie;1.0;8\n" +
+                "gummy;1.17;1\n" +
+                "\n" +
+                "\n" +
+                "cookie;1.0;8\n" +
+                "Chocolate bar;2.45;3\n" +
+                "cupcake;1.30;6\n" +
+                "gummy;1.17;1\n" +
+                "Ice cream;1.90;2\n" +
+                "Protein bar;3.10;1\n" +
+                "Minimal cost = 3.00\n" +
+                "Index of purchase with sum 5.00 : 3\n");
+    }
+
+    @Test
+    void doesSortWork() {
+        Product chocolateBar = new Product("Chocolate bar", new Euro(245));
+
+        AbstractPurchase[] purchases = new AbstractPurchase[]{
+                new DiscountEveryPurchase(chocolateBar, 3),
+                new DiscountEveryPurchase(chocolateBar, 2),
+                new DiscountEveryPurchase(chocolateBar, 4),
+                new DiscountEveryPurchase(chocolateBar, 1)
+        };
+
+        Arrays.sort(purchases);
+
+        for (AbstractPurchase p : purchases )
+            System.out.println(p);
+
+        assertOutEquals("Chocolate bar;2.45;4\n" +
+                "Chocolate bar;2.45;3\n" +
+                "Chocolate bar;2.45;2\n" +
+                "Chocolate bar;2.45;1\n");
+    }
+
+    @Test
+    void doesSearchWork(){
+        Product chocolateBar = new Product("Chocolate bar", new Euro(300));
+
+        Runner runner = new Runner();
+
+        AbstractPurchase[] purchases = new AbstractPurchase[]{
+                new DiscountEveryPurchase(chocolateBar, 3),
+                new DiscountEveryPurchase(chocolateBar, 2),
+                new DiscountEveryPurchase(chocolateBar, 4),
+                new DiscountEveryPurchase(chocolateBar, 1)
+        };
+
+
+        Arrays.sort(purchases);
+
+        assertEquals(runner.search(purchases, new Euro(800)), 1);
+    }
 }
